2da5de3c2a511a476711c727b9b0e4f1521515b6
Illia Lebid <aidonline01@gmail.com>
2024-05-12 22:11:47 +0200

    Done first version

---
+++ main.js
@@ -0,0 +1,108 @@
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+const inputPrice = async () => {
+  return new Promise((done) => {
+    readline.question('What is the price of the flat? \n', price => {
+      done(+price);
+    });
+  });
+}
+
+const inputLabel = async () => {
+  return new Promise(done => {
+    readline.question('What is the energy label? \n', label => {
+      label = label.toUpperCase();
+
+      // If label is unknown, we assume that it is G
+      if(!['A', 'B', 'C', 'D', 'E', 'F', 'G'].includes(label)) {
+        label = 'G';
+      }
+
+      done(label.toUpperCase());
+    });
+  });
+}
+
+const inputTravelTicket = async () => {
+  return new Promise(done => {
+    readline.question('For how many zones do you need to buy ticket? (0 = no need for ticket, 7 = full city ticket) \n', label => {
+      // if ticket is unknown we assume that it is full city ticket
+      if(![0, 1, 2, 3, 4, 5, 6].includes(label)) {
+        label = 'Full city';
+      }
+
+      done(label);
+    });
+  });
+}
+
+const inputMoveInDate = async () => {
+  return new Promise(done => {
+    readline.question('How many months do you have to move in \n', moveInMonths => {
+      done(+moveInMonths);
+    });
+  });
+}
+
+const calculateMovingCost = (monthsLength) => {
+  const pricePerMonth = 1900;
+  const paidMonths = Math.max(0, 2 - monthsLength);
+
+  return pricePerMonth * paidMonths;
+}
+
+const calculateLabelPrice = (label) => {
+  const pricesPerLabel = {
+    'A': 99,
+    'B': 112,
+    'C': 130,
+    'D': 156,
+    'E': 242,
+    'F': 260,
+    'G': 307
+  };
+
+  return pricesPerLabel[label];
+}
+
+const calculateTravelPrice = (travelTicket) => {
+  const pricesPerZonesPerYear = {
+    1: 590,
+    2: 960,
+    3: 1420,
+    4: 1890,
+    5: 2360,
+    6: 2840,
+    'Full city': 3354
+  };
+
+  return pricesPerZonesPerYear[travelTicket] / 12;
+}
+
+const main = async () => {
+  const price = await inputPrice();
+  const label = await inputLabel();
+  const travelTicket = await inputTravelTicket();
+  const monthsUntilMoveIn = await inputMoveInDate();
+
+  const labelPrice = calculateLabelPrice(label);
+  const travelPrice = calculateTravelPrice(travelTicket);
+  const moveInPrice = calculateMovingCost(monthsUntilMoveIn);
+
+  const totalPrice = price + labelPrice + travelPrice + moveInPrice;
+  console.log(price, labelPrice, travelPrice, moveInPrice);
+
+  console.log(`
+    Rent price: ${price} euros,
+    Enegy label: ${label},
+    Travel ticket: ${travelTicket}${travelTicket === 'Full city' ? '' : ' zones'},
+    Total price: ${Math.round(totalPrice)} euros
+  `);
+
+  readline.close();
+};
+
+main();
